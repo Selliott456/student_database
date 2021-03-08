3768d2fe781ac1d0d7edebf1ddf4a4a1fe6ff79f
Sherryll Elliott <sherryllelliott@gmail.com>
2021-03-08 11:54:33 +0000

    basics in student class set up

--- Student.java
+++ Student.java
@@ -1,12 +1,24 @@
-public class student {
-  //constructor: asks user for name and year.
-  
-  //generate an id
+import java.util.Scanner;
 
-  //enroll in courses
+public class Student {
+  private String firstName;
+  private String lastName;
+  private int gradeYear;
+  private int studentId;
+  private String courses;
+  private int balance;
+  private int costOfCourse = 600;
 
-  //view balance and pay tuition
+    //constructor: asks user for name and year.
+  public Student() {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter student first name: ");
+      this.firstName = in.nextLine();
 
-  //student status
-  
+      System.out.print("Enter student last name: ");
+      this.lastName = in.nextLine();
+
+      System.out.print("Enter student year (1-4): ");
+      this.gradeYear = in.nextInt();
+  }
 }
--- Studentdatabase.java
+++ Studentdatabase.java
@@ -1,6 +1,6 @@
 class Studentdatabase {
   public static void main(String[] args) {
-      // ask how many users to add
-      //create X number of new students
-  }
+      Student student1 = new Student();
+      }
+  
 }
