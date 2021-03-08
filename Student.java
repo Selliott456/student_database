29aea880734d2b2c16df0a71a7a88c92c4e4032b
Sherryll Elliott <sherryllelliott@gmail.com>
2021-03-08 11:42:45 +0000

    bones and pseudo code laid out

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class student {
+  //constructor: asks user for name and year.
+  
+  //generate an id
+
+  //enroll in courses
+
+  //view balance and pay tuition
+
+  //student status
+  
+}
---
+++ Studentdatabase.java
@@ -0,0 +1,6 @@
+class Studentdatabase {
+  public static void main(String[] args) {
+      // ask how many users to add
+      //create X number of new students
+  }
+}
